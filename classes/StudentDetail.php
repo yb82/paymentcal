3c12cd456deb6c347878f9da2358c42c003f88f1
yb82 <yb82@naver.com>
2017-04-04 06:42:51 +1000

    Receive post data

---
+++ classes/StudentDetail.php
@@ -0,0 +1,22 @@
+<?php
+
+	class StudentDetail {
+		public $titleType  = array("Mr." ,"Ms", "Miss","Mrs" );
+	public $gender = array('Male' ,'Female' );
+
+	public $title;
+	public $fName;
+	public $lName;
+	//public $gender;
+	public $dateOfBirth;
+	public $nation;
+	public $passportNo;
+	public $currentAddr;
+	public $contact;
+	public $email;
+	public $insurance;
+	public function __construct(){
+			//$this->courseStartDates= array();
+		
+		}
+}
